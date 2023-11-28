df7324e1268d0f32042786d05f57be074f1dad34
Lucas Apparecido de Oliveira <108204218+LucasApparecido@users.noreply.github.com>
2023-11-27 21:36:59 -0300

    foiei

--- SQL(Criação)/DDL_Hospital.sql
+++ SQL(Criação)/DDL_Hospital.sql
@@ -174,39 +174,20 @@ create table internacao_medic (
 );
 
 
-drop table convenio;
-drop table paciente;
-drop table endereco;
-drop table especialidade;
-drop table medico;
-drop table medico_esp;
-drop table convenio_pac;
-drop table convenio_med;
-drop table consulta;
-drop table exame;
-drop table medicamento;
-drop table quarto;
-drop table internacao;
-drop table consulta_exa;
-drop table consulta_medic;
-drop table internacao_exa;
 drop table internacao_medic;
-
-
-drop table convenio;
-drop table paciente;
-drop table endereco;
-drop table especialidade;
-drop table medico;
-drop table medico_esp;
-drop table convenio_pac;
-drop table convenio_med;
-drop table consulta;
-drop table exame;
-drop table medicamento;
-drop table quarto;
-drop table internacao;
-drop table consulta_exa;
-drop table consulta_medic;
 drop table internacao_exa;
-drop table internacao_medic;
+drop table consulta_medic;
+drop table consulta_exa;
+drop table internacao;
+drop table quarto;
+drop table medicamento;
+drop table exame;
+drop table consulta;
+drop table convenio_med;
+drop table convenio_pac;
+drop table medico_esp;
+drop table medico;
+drop table especialidade;
+drop table endereco;
+drop table paciente;
+drop table convenio;
